e1c2f6a54672ccc8c4c3fc6ec0d0a41e53f8da7b
KANDUKURI SAI KRISHNA <kandukurisaikrishna@Dheerajs-MacBook-Air-3.local>
2026-02-14 03:25:19 +0530

    Update configs for custom domain and deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
+
 import { inspectorServer } from '@react-dev-inspector/vite-plugin';
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
-
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: 'KANDUKURIsaikrishna/My_Portfolio/', // Replace with your repo name
   server: {
     host: "::",
     port: 8080,
@@ -15,7 +16,7 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-  mode === "development" && inspectorServer(),
+    mode === "development" && inspectorServer(),
   ].filter(Boolean),
   resolve: {
     alias: {
